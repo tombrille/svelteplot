99b08bec7c625c508da8611a00fb4ea91a96ae10
Gregor Aisch <gka@users.noreply.github.com>
2025-01-29 23:45:44 +0100

    refactor Arrow mark

--- src/lib/marks/Arrow.svelte
+++ src/lib/marks/Arrow.svelte
@@ -12,9 +12,8 @@
         FacetContext,
         RawValue
     } from '../types.js';
-    import { resolveChannel, resolveProp, resolveScaledStyles } from '../helpers/resolve.js';
-    import { coalesce, maybeData, testFilter, maybeNumber } from '../helpers/index.js';
-    import { projectXY } from '../helpers/scales.js';
+    import { resolveChannel, resolveProp, resolveStyles } from '../helpers/resolve.js';
+    import { coalesce, maybeData, maybeNumber } from '../helpers/index.js';
     import Mark from '../Mark.svelte';
     import { arrowPath, maybeSweep, type SweepOption } from '../helpers/arrowPath.js';
     import { replaceChannels } from '$lib/transforms/rename.js';
@@ -60,13 +59,9 @@
     let { data = [{}], class: className = null, ...options }: ArrowProps = $props();
 
     const { getPlotState } = getContext<PlotContext>('svelteplot');
-    let plot = $derived(getPlotState());
+    const plot = $derived(getPlotState());
 
-    function isValid(value: RawValue): value is number | Date | string {
-        return value !== null && (typeof value === 'string' || !Number.isNaN(value));
-    }
-
-    let sorted = $derived(
+    const sorted = $derived(
         options.sort
             ? maybeData(data).toSorted((a, b) =>
                   resolveChannel('sort', a, options) > resolveChannel('sort', b, options) ? 1 : -1
@@ -74,12 +69,9 @@
             : maybeData(data)
     );
 
-    let args: ArrowProps = $derived(
+    const args: ArrowProps = $derived(
         replaceChannels({ data: sorted, ...options }, { y: ['y1', 'y2'], x: ['x1', 'x2'] })
     );
-
-    const { getTestFacet } = getContext<FacetContext>('svelteplot/facet');
-    let testFacet = $derived(getTestFacet());
 </script>
 
 <Mark
@@ -87,81 +79,59 @@
     required={['x1', 'x2', 'y1', 'y2']}
     channels={['x1', 'y1', 'x2', 'y2', 'opacity', 'stroke', 'strokeOpacity']}
     {...args}>
-    {#snippet children({ mark, usedScales })}
+    {#snippet children({ mark, usedScales, scaledData })}
         {@const sweep = maybeSweep(args.sweep)}
-        <GroupMultiple class="arrow" length={args.data.length}>
-            {#each args.data as datum}
-                {#if testFilter(datum, args) && testFacet(datum, mark.options)}
-                    {@const _x1 = resolveChannel('x1', datum, args)}
-                    {@const _x2 = resolveChannel('x2', datum, args)}
-                    {@const _y1 = resolveChannel('y1', datum, args)}
-                    {@const _y2 = resolveChannel('y2', datum, args)}
-                    {@const strokeWidth = resolveProp(args.strokeWidth, datum, 1)}
-                    {#if isValid(_x1) && isValid(_x2) && isValid(_y1) && isValid(_y2)}
-                        {@const [x1, y1] = projectXY(
-                            plot.scales,
-                            _x1,
-                            _y1,
-                            usedScales.x1,
-                            usedScales.y1
-                        )}
-                        {@const [x2, y2] = projectXY(
-                            plot.scales,
-                            _x2,
-                            _y2,
-                            usedScales.x2,
-                            usedScales.y2
-                        )}
-                        {@const dx = resolveProp(args.dx, datum, 0)}
-                        {@const dy = resolveProp(args.dx, datum, 0)}
-                        {@const inset = resolveProp(args.inset, datum, 0)}
-                        {@const insetStart = resolveProp(args.insetStart, datum)}
-                        {@const insetEnd = resolveProp(args.insetEnd, datum)}
-                        {@const headAngle = resolveProp(args.headAngle, datum, 60)}
-                        {@const headLength = resolveProp(args.headLength, datum, 8)}
-                        {@const bend = resolveProp(args.bend, datum, 0)}
-                        {@const arrPath = arrowPath(
-                            x1,
-                            y1,
-                            x2,
-                            y2,
-                            maybeNumber(coalesce(insetStart, inset)),
-                            maybeNumber(coalesce(insetEnd, inset)),
-                            headAngle,
-                            headLength,
-                            bend === true ? 22.5 : bend === false ? 0 : bend,
-                            strokeWidth,
-                            sweep
-                        )}
-                        <g
-                            class={className}
-                            data-y1={_y1}
-                            data-y1_={y1}
-                            transform={dx || dy ? `translate(${dx}, ${dy})` : null}
-                            use:addEventHandlers={{
-                                getPlotState,
-                                options: mark.options,
-                                datum
-                            }}>
-                            {#if options.onmouseenter || options.onclick}
-                                <!-- add invisible path in bg for easier mouse access -->
-                                <path
-                                    d={arrPath}
-                                    style="fill:none;stroke-width: {(strokeWidth || 1) +
-                                        10}; stroke: red; stroke-opacity:0" />
-                            {/if}
+        <GroupMultiple class="arrow" length={scaledData.length}>
+            {#each scaledData as d}
+                {#if d.valid}
+                    {@const inset = resolveProp(args.inset, d.datum, 0)}
+                    {@const insetStart = resolveProp(args.insetStart, d.datum)}
+                    {@const insetEnd = resolveProp(args.insetEnd, d.datum)}
+                    {@const headAngle = resolveProp(args.headAngle, d.datum, 60)}
+                    {@const headLength = resolveProp(args.headLength, d.datum, 8)}
+                    {@const bend = resolveProp(args.bend, d.datum, 0)}
+                    {@const strokeWidth = resolveProp(args.strokeWidth, d.datum, 1)}
+                    {@const arrPath = arrowPath(
+                        d.x1,
+                        d.y1,
+                        d.x2,
+                        d.y2,
+                        maybeNumber(coalesce(insetStart, inset)),
+                        maybeNumber(coalesce(insetEnd, inset)),
+                        headAngle,
+                        headLength,
+                        bend === true ? 22.5 : bend === false ? 0 : bend,
+                        strokeWidth,
+                        sweep
+                    )}
+                    {@const [style, styleClass] = resolveStyles(
+                        plot,
+                        d,
+                        { ...args, strokeWidth },
+                        'stroke',
+                        usedScales
+                    )}
+                    <g
+                        class={[className]}
+                        use:addEventHandlers={{
+                            getPlotState,
+                            options: mark.options,
+                            datum: d.datum
+                        }}>
+                        {#if options.onmouseenter || options.onclick}
+                            <!-- add invisible path in bg for easier mouse access -->
                             <path
                                 d={arrPath}
-                                style={resolveScaledStyles(
-                                    datum,
-                                    args,
-                                    usedScales,
-                                    plot,
-                                    'stroke'
-                                )} />
-                        </g>
-                    {/if}
+                                style="fill:none;stroke-width: {(strokeWidth || 1) +
+                                    10}; stroke: red; stroke-opacity:0" />
+                        {/if}
+                        <path
+                            class={[styleClass]}
+                            d={arrPath}
+                            {style} />
+                    </g>
                 {/if}
+               
             {/each}
         </GroupMultiple>
     {/snippet}
