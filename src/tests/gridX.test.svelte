3e15aba7070d27871e13b8bc174f61b283965b39
hobbes7878 <Jon.McClure@thomsonreuters.com>
2025-05-09 14:20:02 +0100

    couple simple tests that _should_ pass with #3 closed

---
+++ src/tests/gridX.test.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { GridX, Plot } from 'svelteplot';
+  let { plotArgs, gridArgs } = $props();
+</script>
+
+<Plot width={100} height={100} axes={false} {...plotArgs}>
+  <GridX {...gridArgs} />
+</Plot>
---
+++ src/tests/gridY.test.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { GridY, Plot } from 'svelteplot';
+  let { plotArgs, gridArgs } = $props();
+</script>
+
+<Plot width={100} height={100} axes={false} {...plotArgs}>
+  <GridY {...gridArgs} />
+</Plot>
