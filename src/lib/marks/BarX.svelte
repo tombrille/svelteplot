6c216d06b5f670d8a85cde7df0e043baf6443894
gka <gka@users.noreply.github.com>
2025-05-18 14:09:56 +0200

    refactor BarX

--- src/lib/marks/BarX.svelte
+++ src/lib/marks/BarX.svelte
@@ -6,13 +6,11 @@
     import Mark from '../Mark.svelte';
     import { getContext } from 'svelte';
     import { stackX, recordizeX, intervalX, sort } from '$lib/index.js';
-    import { resolveProp, resolveStyles } from '../helpers/resolve.js';
-    import { roundedRect } from '../helpers/roundedRect.js';
     import type { PlotContext, BaseMarkProps, RectMarkProps, ChannelAccessor } from '../types.js';
     import type { StackOptions } from '$lib/transforms/stack.js';
     import type { DataRow } from '$lib/types.js';
-    import { addEventHandlers } from './helpers/events.js';
     import GroupMultiple from './helpers/GroupMultiple.svelte';
+    import RectPath from './helpers/RectPath.svelte';
 
     type BarXProps = BaseMarkProps & {
         data: DataRow[];
@@ -21,14 +19,6 @@
         x2?: ChannelAccessor;
         y?: ChannelAccessor;
         stack?: StackOptions;
-        borderRadius?:
-            | number
-            | {
-                  topLeft?: number;
-                  topRight?: number;
-                  bottomRight?: number;
-                  bottomLeft?: number;
-              };
     } & RectMarkProps;
 
     let { data = [{}], class: className = null, stack, ...options }: BarXProps = $props();
@@ -59,33 +49,17 @@
                 {@const bw = plot.scales.y.fn.bandwidth()}
                 {@const minx = Math.min(d.x1, d.x2)}
                 {@const maxx = Math.max(d.x1, d.x2)}
-                {@const insetLeft = resolveProp(args.insetLeft, d.datum, 0)}
-                {@const insetRight = resolveProp(args.insetRight, d.datum, 0)}
-                {@const insetTop = resolveProp(args.insetTop || args.inset, d.datum, 0)}
-                {@const insetBottom = resolveProp(args.insetBottom || args.inset, d.datum, 0)}
-                {@const dx = resolveProp(args.dx, d.datum, 0)}
-                {@const dy = resolveProp(args.dy, d.datum, 0)}
                 {#if d.valid}
-                    {@const [style, styleClass] = resolveStyles(plot, d, args, 'fill', usedScales)}
-                    <path
-                        d={roundedRect(
-                            0,
-                            0,
-                            maxx - minx - insetLeft - insetRight,
-                            bw - insetTop - insetBottom,
-                            options.borderRadius
-                        )}
-                        class={[styleClass, className]}
-                        {style}
-                        transform="translate({[
-                            minx + dx + insetLeft,
-                            d.y + insetTop + dy - bw * 0.5
-                        ]})"
-                        use:addEventHandlers={{
-                            getPlotState,
-                            options: args,
-                            datum: d.datum
-                        }} />
+                    <RectPath
+                        {usedScales}
+                        class={className}
+                        {options}
+                        datum={d}
+                        x={minx}
+                        fallbackInsetH={false}
+                        y={d.y - bw * 0.5}
+                        width={maxx - minx}
+                        height={bw} />
                 {/if}
             {/each}
         </GroupMultiple>
