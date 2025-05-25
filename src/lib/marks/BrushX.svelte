c0c025db443666a83c639f7c003a3c6a637ba355
gka <gka@users.noreply.github.com>
2025-05-25 23:49:53 +0200

    chore: refine & export brush mark prop types

--- src/lib/marks/BrushX.svelte
+++ src/lib/marks/BrushX.svelte
@@ -1,7 +1,11 @@
+<script module type="ts">
+    export type BrushXMarkProps = Omit<BrushMarkProps, 'limitDimension'>;
+</script>
+
 <script lang="ts">
     import Brush, { type BrushMarkProps } from './Brush.svelte';
 
-    let { brush, ...restProps }: Omit<BrushMarkProps, 'limitDimension'> = $props();
+    let { brush, ...restProps }: BrushXMarkProps = $props();
 </script>
 
 <Brush bind:brush limitDimension="x" {...restProps} />
