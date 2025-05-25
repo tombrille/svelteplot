35cc427d3695a9649a1af958c7f6c82f72da02f3
gka <gka@users.noreply.github.com>
2025-05-25 18:31:44 +0200

    refine graticule mark props

--- src/lib/marks/Graticule.svelte
+++ src/lib/marks/Graticule.svelte
@@ -1,19 +1,23 @@
+<script module lang="ts">
+    import type { DefaultOptions, BaseMarkProps } from '../types.js';
+    export type GraticuleMarkProps = Omit<
+        BaseMarkProps,
+        'fill' | 'fillOpacity' | 'paintOrder' | 'title' | 'href' | 'target' | 'cursor'
+    > & {
+        step?: number;
+        stepX?: number;
+        stepY?: number;
+    };
+</script>
+
 <script lang="ts">
     import Geo from './Geo.svelte';
     import { geoGraticule } from 'd3-geo';
-    import type { DefaultOptions, BaseMarkProps } from '../types.js';
-
     import { getContext } from 'svelte';
 
     const DEFAULTS = {
         graticuleStep: 10,
         ...getContext<Partial<DefaultOptions>>('svelteplot/defaults')
-    };
-
-    type GraticuleMarkProps = BaseMarkProps & {
-        step?: number;
-        stepX?: number;
-        stepY?: number;
     };
 
     let { step = DEFAULTS.graticuleStep, stepX, stepY, ...options }: GraticuleMarkProps = $props();
