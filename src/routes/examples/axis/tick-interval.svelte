effd7aba2c0e4e4dadf671cc6e888b0f43c67498
gka <gka@users.noreply.github.com>
2025-05-29 12:41:38 +0200

    add tick interval example

---
+++ src/routes/examples/axis/tick-interval.svelte
@@ -0,0 +1,15 @@
+<script module>
+    export const title = 'Tick interval';
+</script>
+
+<script>
+    import { Plot, AxisX, AxisY, Line } from 'svelteplot';
+    import { page } from '$app/state';
+    let { aapl } = $derived(page.data.data);
+</script>
+
+<Plot>
+    <AxisX interval="6 months" />
+    <AxisY interval={25} />
+    <Line data={aapl} x="Date" y="Close" />
+</Plot>
