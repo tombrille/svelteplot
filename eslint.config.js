54ca574e11794229c6b59a9be13521acdca8d89f
gka <gka@users.noreply.github.com>
2025-05-29 16:30:45 +0200

    don't allow inspect and console.log

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,17 @@ import svelteParser from 'svelte-eslint-parser';
 import tsParser from '@typescript-eslint/parser';
 
 export default [
+    {
+        rules: {
+            'no-console': ['error', { allow: ['error'] }]
+        }
+    },
     ...svelte.configs.recommended,
     {
         files: ['**/*.svelte', '*.svelte'],
         rules: {
-            'svelte/no-object-in-text-mustaches': 'warn'
+            'svelte/no-object-in-text-mustaches': 'warn',
+            'svelte/no-inspect': 'warn'
         },
         ignores: ['dist/*', '.sveltepress/*'],
         languageOptions: {
