edb7ba3c5c3c9fde1caf752f4767f4c5211dfebe
gka <gka@users.noreply.github.com>
2025-06-02 23:35:06 +0200

    docs: add example for linked bars

---
+++ src/routes/examples/bar/linked-bars.svelte
@@ -0,0 +1,17 @@
+<script module>
+    export const title = 'Linked Bars';
+    export const description =
+        'A bar chart with linked bars, showing the relationship between two datasets.';
+</script>
+
+<script>
+    import { Plot, BarX } from 'svelteplot';
+    import { page } from '$app/state';
+</script>
+
+<Plot>
+    <BarX
+        data={[1, 2, 3, 4, 5]}
+        href={(d) => `#/${d}`}
+        target="_blank" />
+</Plot>
