26dba292e698e78047830a1be796d49fc86c7df9
gka <gka@users.noreply.github.com>
2025-06-02 23:34:25 +0200

    chore: wrap <a> in Anchor helper component

---
+++ src/lib/marks/helpers/Anchor.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { resolveProp } from '$lib/helpers/resolve';
+
+    let { datum = {}, options = {}, children } = $props();
+
+    const href = $derived(resolveProp(options.href, datum, null));
+    const target = $derived(resolveProp(options.target, datum, null));
+    const rel = $derived(resolveProp(options.rel, datum, null));
+    const type = $derived(resolveProp(options.type, datum, null));
+    const download = $derived(resolveProp(options.download, datum, null));
+</script>
+
+{#if href}
+    <!-- we can't use <a> directly here because Svelte confuses it with the 
+ HTMLAElement which breaks the rendering -->
+    <svelte:element
+        this={'a'}
+        {href}
+        {target}
+        {rel}
+        {type}
+        {download}
+        aria-label="link"
+        xmlns="http://www.w3.org/2000/svg">
+        {@render children?.()}
+    </svelte:element>
+{:else}
+    {@render children?.()}
+{/if}
