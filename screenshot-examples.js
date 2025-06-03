100bb88bc0bb879513f5ce3b46ca67ebbbab404d
gka <gka@users.noreply.github.com>
2025-06-03 08:33:39 +0200

    redo screenshots

--- screenshot-examples.js
+++ screenshot-examples.js
@@ -94,7 +94,9 @@ const takeScreenshot = async (page, urlPath, outputPath, isDarkMode = false) => 
     const finalOutputPath = outputPath.replace('.png', `${themeSuffix}.png`);
 
     // Wait for the Plot component to be rendered
-    await page.waitForSelector('.content > figure.svelteplot', { timeout: 10000 });
+    await page.waitForSelector('.content figure.svelteplot ', {
+        timeout: 10000
+    });
 
     // Toggle dark mode if needed
     if (isDarkMode) {
@@ -112,7 +114,7 @@ const takeScreenshot = async (page, urlPath, outputPath, isDarkMode = false) => 
 
     // Get the Plot SVG element
     const elementHandle = await page.evaluateHandle(() =>
-        document.querySelector('.content > figure.svelteplot > .plot-body > svg')
+        document.querySelector('.content .screenshot')
     );
 
     // Take a screenshot of the element
