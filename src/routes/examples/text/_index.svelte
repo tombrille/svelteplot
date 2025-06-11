03e74cba098230967ff4517e83a85ee26c4bc38b
gka <gka@users.noreply.github.com>
2025-06-11 13:11:22 +0200

    docs: add text example

---
+++ src/routes/examples/text/_index.svelte
@@ -0,0 +1,5 @@
+<script module>
+    export const title = 'Text';
+</script>
+
+<h1>Text examples</h1>
---
+++ src/routes/examples/text/frame-anchor.svelte
@@ -0,0 +1,32 @@
+<script module>
+    export const title = 'Frame Anchor';
+</script>
+
+<script lang="ts">
+    import { setContext } from 'svelte';
+
+    import { Plot, Text } from 'svelteplot';
+    import type { PlotDefaults } from 'svelteplot/types';
+    setContext<Partial<PlotDefaults>>(
+        'svelteplot/defaults',
+        {
+            frame: {
+                strokeOpacity: 0.25
+            }
+        }
+    );
+</script>
+
+<Plot
+    frame
+    height={300}
+    axes={false}
+    x={{ domain: [0, 10] }}
+    y={{ domain: [0, 10] }}>
+    {#each ['top', 'bottom', 'center', 'left', 'right', 'top-left', 'top-right', 'bottom-left', 'bottom-right'] as frameAnchor}
+        <Text
+            {frameAnchor}
+            fontSize={18}
+            text={frameAnchor} />
+    {/each}
+</Plot>
