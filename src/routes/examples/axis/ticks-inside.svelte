82f69aeee152470dec78e038e9c0709a458f8f54
gka <gka@users.noreply.github.com>
2025-06-18 08:29:39 +0200

    docs: add ticks inside example

---
+++ src/routes/examples/axis/ticks-inside.svelte
@@ -0,0 +1,24 @@
+<script module>
+    export const title = 'Ticks inside';
+</script>
+
+<script>
+    import { Plot, AxisY, Line } from 'svelteplot';
+    import { page } from '$app/state';
+
+    let { aapl } = $derived(page.data.data);
+</script>
+
+<Plot
+    marginTop={30}
+    x={{ insetLeft: 25 }}
+    y={{ grid: true }}>
+    <AxisY
+        tickSize={0}
+        tickPadding={0}
+        dy={-5}
+        title={false}
+        lineAnchor="bottom"
+        textAnchor="start" />
+    <Line data={aapl} x="Date" y="Close" />
+</Plot>
